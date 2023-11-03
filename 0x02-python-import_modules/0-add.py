e5f7ee768ba873ee805be567e99869f6f2eb304f
Abanob <abanobm94@gmail.com>
2023-11-02 18:05:31 -0700

    2JZ

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
+if __name__ == "__main__":
+    from add_0 import add
 
-    count = len(sys.argv) -1
-    if count == 0:
-        print('0 argument.')
-    elif count == 1:
-        print('1 argument:')
-    else:
-        print('() argument:'.format(count))
-    for i in range(count):
-        print("{}:  {} ".format(i + 1, sys.argv[i + 1]))
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
